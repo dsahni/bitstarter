f7a7e7e07946274671d74d4b8f83ab42c1a8fd89
Dhruv Sahni <dhruvsahni@gmail.com>
2013-07-09 16:46:17 -0400

    change

--- web.js
+++ web.js
@@ -6,8 +6,6 @@ app.get('/', function(request, response) {
   response.send('Hello World from index.html');
 });
 
-fs.readFileSync(index.html)
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
